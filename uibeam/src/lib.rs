9589af3b0f38a053f9399183277c88556ec865e9
kanarus <kanarus786@gmail.com>
2025-04-08 01:56:00 +0900

    2025-04-08 01:55+9:00

--- uibeam/src/lib.rs
+++ uibeam/src/lib.rs
@@ -1,44 +1,83 @@
 pub struct UI(std::borrow::Cow<'static, str>);
 impl UI {
-    pub fn new<I>(
-        template: &'static str,
-        interpolation: impl Interpolations<I>,
-    ) -> Result<Self, UIBeamError> {
+    #[doc(hidden)]
+    /// tends to be used by the `UI!` macro internally.
+    /// 
+    /// ## SAFETY
+    /// 
+    /// 1. `template_pieces` must not be empty.
+    /// 2. `template_pieces`'s length must be `N + 1`, where
+    ///    `N` is the length of `expressions` to be interpolated.
+    /// 3. `template_pieces` must `
+    pub unsafe fn new_unchecked<const N: usize>(
+        template_pieces: &'static [&'static str],
+        expressions: [UI; N],
+    ) -> Self {
+        if template_pieces.len() - 1 != expressions.len() {
+            // return Err(UIBeamError::InterpolationMismatch {
+            //     expected: template_pieces.len() - 1,
+            //     found: expressions.len(),
+            // });
+            unreachable!("UI! macro should ensure this never happens");
+        }
+
         todo!()
     }
 }
+// `expressions` should be able to be either
+// 
+// 1. children UI(s)  : HTML nodes (including text nodes)
+// 2. attribute value : escaped text value
+// 
+// so, current `[UI; N]` is not enough.
 
-pub struct UIBeamError {
-    pub message: String,
-    pub line: usize,
-    pub column: usize,
+pub enum UIBeamError {
+    Html(
+        uibeam_html::Error,
+    ),
+    InterpolationMismatch {
+        expected: usize,
+        found: usize,
+    },
 }
 
-/// A collection of `Expression` types to complete the `UI` template
-/// with interpolations.
-pub trait Interpolations<I> {
-    // fn interpolate(self, template: uibeam_html::Template) -> Result<UI, UIBeamError>;
-}
-impl Interpolations<()> for () {}
+pub enum Expression {
+    AttributeValue(std::borrow::Cow<'static, str>),
+    Children(Vec<UI>),
+} // should not be an unified enum, but rather ...
 
-// impl<E1: Ex> Interpolations 
-
-pub trait Expression<T> {
-    // fn eval(self) -> Result<uibeam_html::Node, UIBeamError>;
+pub trait Interpolator<X> {
+    fn into_expression(self) -> Expression;
 }
-impl Expression<&'static str> for &'static str {}
-impl Expression<String> for String {}
-// impl Expression for uibeam_html::Node {}
-impl<I> Expression<(I,)> for I
+
+impl Interpolator<&'static str> for &'static str {
+    fn into_expression(self) -> Expression {
+        Expression::AttributeValue(std::borrow::Cow::Borrowed(self))
+    }
+}
+impl Interpolator<String> for String {
+    fn into_expression(self) -> Expression {
+        Expression::AttributeValue(std::borrow::Cow::Owned(self))
+    }
+}
+impl<T, X> Interpolator<Option<X>> for Option<T>
 where
-    I: IntoIterator<Item = UI>,
+    T: Interpolator<X>,
+{
+
+}
+impl<I> Interpolator<(I,)> for I
+where
+    I: Iterator<Item = UI>,
 {}
 
 #[cfg(test)]
 fn assert_impls() {
-    fn is_expression<T, E: Expression<T>>(_: E) {}
+    fn is_expression<T, E: Interpolation<T>>(_: E) {}
+    fn dummy_ui() -> UI {todo!()}
 
     is_expression("Hello, UI Beam!");
     is_expression(format!("Hello, {}!", "UI Beam"));
     is_expression(Some("Hello, UI Beam!"));
+    is_expression((1..=3).map(|_| dummy_ui()));
 }
--- uibeam_html/src/lib.rs
+++ uibeam_html/src/lib.rs
@@ -1,8 +1,10 @@
+pub enum Error {}
+
 /// incomplete HTML nodes, maybe including placeholders to render Rust expressions
 pub struct Template {
     // todo
 }
 
-pub struct Node {
-    // todo
+pub enum Node {
+    Text(std::borrow::Cow<'static, str>),
 }
