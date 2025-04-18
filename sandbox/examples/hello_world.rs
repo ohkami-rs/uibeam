60460b7572c58aae360aad29785e1a624f9d65ec
kanarus <kanarus786@gmail.com>
2025-04-19 04:11:48 +0900

    update hello_world

--- sandbox/examples/hello_world.rs
+++ sandbox/examples/hello_world.rs
@@ -3,12 +3,13 @@ use uibeam::UI;
 fn main() {
     let name = "world";
     let style = "color: red; font-size: 20px;";
+    let user_name = "Mr.<script>alert('XSS');</script>";
 
     println!("{}", uibeam::shoot(UI! {
         <div class="hello" style={style}>
             "Hello "{name}"!"
             <br/>
-            "こんにちは" //@{name}!
+            "こんにちは @"{user_name}"!"
         </div>
     }));
 }
