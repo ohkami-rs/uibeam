646ec24f691f4b58e2c369d626d6deaa278eefe8
kanarus <kanarus786@gmail.com>
2025-04-26 01:12:29 +0900

    2025-04-26 01:12+9:00

--- uibeam-vscode/src/extension.ts
+++ uibeam-vscode/src/extension.ts
@@ -1,1 +1,28 @@
-// todo
+import {
+    commands,
+    workspace,
+    DocumentFilter,
+    ExtensionContext,
+} from 'vscode';
+import {
+    getLanguageService,
+} from 'vscode-html-languageservice';
+
+export function activate(context: ExtensionContext) {
+    const htmlLS = getLanguageService();
+
+    const rust: DocumentFilter = {
+        scheme: 'file',
+        language: 'rust',
+        pattern: '**/*.rs',
+    };
+
+    context.subscriptions.push(workspace.registerTextDocumentContentProvider(
+        '',
+        {
+            provideTextDocumentContent(uri, token) {
+                
+            },
+        }
+    ));
+}
