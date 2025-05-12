d834f0b1c19b264c470774d187d5cd0c6d0ece9c
kanarus <kanarus786@gmail.com>
2025-05-12 10:04:20 +0900

    fix examples/tailwind

--- examples/tailwind/src/main.rs
+++ examples/tailwind/src/main.rs
@@ -13,7 +13,7 @@ impl Beam for Layout {
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <link rel="stylesheet" href="./tailwind.css">
-                <title>"Ohkami organization"</title>
+                <title>"UIBeam with TailwindCSS sample"</title>
             </head>
             <body>
                 {self.children}
