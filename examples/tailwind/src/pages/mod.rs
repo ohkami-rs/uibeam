91df618871b2c91c56934987d156cbb74078a07a
kanarus <kanarus786@gmail.com>
2025-05-12 10:01:51 +0900

    fix examples/tailwind

--- examples/tailwind/src/pages/mod.rs
+++ examples/tailwind/src/pages/mod.rs
@@ -3,5 +3,7 @@ mod index;
 pub(super) const fn pages() -> &'static [(&'static str, fn() -> uibeam::UI)] {
     &[
         ("/", index::page),
+        ("/test", index::page),
+        ("/test/test2", index::page),
     ]
 }
