bc72ef2c6b7989de7423192bb554c63567a6f099
kanarus <kanarus786@gmail.com>
2025-05-17 17:05:09 +0900

    perf(UI): eliminate concat doctype (#85)

--- uibeam_macros/src/ui/mod.rs
+++ uibeam_macros/src/ui/mod.rs
@@ -7,16 +7,18 @@ use quote::quote;
 pub(super) fn expand(input: TokenStream) -> syn::Result<TokenStream> {
     let parse::UITokens { nodes } = syn::parse2(input)?;
 
-    let doctype_insertion = nodes.first().map(|node| match node {
+    let mut should_insert_doctype = nodes.first().is_some_and(|node| match node {
         /* starting with <html>..., without <!DOCTYPE html> */        
-        parse::NodeTokens::EnclosingTag { tag, .. } if tag.to_string() == "html" => Some(quote! {
-            unsafe {::uibeam::UI::new_unchecked(&["<!DOCTYPE html>"], [])},
-        }),
-        _ => None,
-    }).flatten();
+        parse::NodeTokens::EnclosingTag { tag, .. } if tag.to_string() == "html" => true,
+        _ => false,
+    });
 
     let nodes = nodes.into_iter().map(|node| {
-        let (literals, expressions) = transform::transform(node);
+        let (mut literals, expressions) = transform::transform(node);
+        if should_insert_doctype {
+            literals.first_mut().unwrap().edit(|lit| *lit = format!("<!DOCTYPE html>{lit}"));
+            should_insert_doctype = false;
+        }
         quote! {
             unsafe {::uibeam::UI::new_unchecked(
                 &[#(#literals),*],
@@ -26,9 +28,6 @@ pub(super) fn expand(input: TokenStream) -> syn::Result<TokenStream> {
     });
 
     Ok(quote! {
-        <::uibeam::UI>::concat([
-            #doctype_insertion
-            #(#nodes),*
-        ])
+        <::uibeam::UI>::concat([#(#nodes),*])
     })
 }
