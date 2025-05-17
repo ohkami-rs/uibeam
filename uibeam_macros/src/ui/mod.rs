a9b5a49d7a94e1c4a62c9c818702a29be30b05ab
kanarus <kanarus786@gmail.com>
2025-05-17 16:37:35 +0900

    fix unwrap and unit test

--- uibeam_macros/src/ui/mod.rs
+++ uibeam_macros/src/ui/mod.rs
@@ -7,13 +7,13 @@ use quote::quote;
 pub(super) fn expand(input: TokenStream) -> syn::Result<TokenStream> {
     let parse::UITokens { nodes } = syn::parse2(input)?;
 
-    let doctype_insertion = match nodes.first().unwrap() {
+    let doctype_insertion = nodes.first().map(|node| match node {
         /* starting with <html>..., without <!DOCTYPE html> */        
         parse::NodeTokens::EnclosingTag { tag, .. } if tag.to_string() == "html" => Some(quote! {
             unsafe {::uibeam::UI::new_unchecked(&["<!DOCTYPE html>"], [])},
         }),
         _ => None,
-    };
+    }).flatten();
 
     let nodes = nodes.into_iter().map(|node| {
         let (literals, expressions) = transform::transform(node);
