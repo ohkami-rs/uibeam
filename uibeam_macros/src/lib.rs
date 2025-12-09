6e737636c49e99df518e6aea8d427f01ce9c919b
kanarus <kanarus786@gmail.com>
2025-12-09 19:36:12 +0900

    docs: unify parentheses format (#144)

--- uibeam_macros/src/lib.rs
+++ uibeam_macros/src/lib.rs
@@ -50,10 +50,10 @@ fn cfg_hydrate() -> bool {
 /// ### Text Nodes
 ///
 /// - _string literals_ : Any string literals are allowed. No `{}` is needed.
-///   - Raw string literals ( `r#"..."#` ) are **NOT escaped**.
+///   - Raw string literals (`r#"..."#`)are **NOT escaped**.
 /// - _interpolations_ : Rust expressions surrounded by `{}` :
 ///   - Any type that implements `std::fmt::Display` is allowed.
-///   - Unsafe blocks ( `unsafe { ... }` ) are **NOT escaped**.
+///   - Unsafe blocks (`unsafe{ ... }`) are **NOT escaped**.
 ///
 /// ### Beams
 ///
